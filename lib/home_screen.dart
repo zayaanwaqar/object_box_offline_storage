8035bb940fd04b523763036b6569fd253422a6a8
zayaan <zayaanwaqar@gmail.com>
2023-11-23 17:49:07 +0500

    Completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:object_box_practice/waitlist_emails/waitlist_emails_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -17,16 +18,16 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final String apiUrl =
-      'https://mymindguide.bubbleapps.io/version-test/api/1.1/wf/create_email';
-  late Map<String, dynamic> data;
+      'https://takatari-cleanup.bubbleapps.io/version-test/api/1.1/wf/create_email_copy';
+
   final TextEditingController controller = TextEditingController();
   ConnectivityResult _connectionStatus = ConnectivityResult.none;
   final Connectivity _connectivity = Connectivity();
   late StreamSubscription<ConnectivityResult> _connectivitySubscription;
-  late SharedPreferences prefs;
-  int i = 0;
 
-  // final Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+  // int dataCount = objectBox.getData()?.length ?? 0;
+  List<Results> waitListEmails = [];
+
   @override
   initState() {
     super.initState();
@@ -36,29 +37,62 @@ class _HomeScreenState extends State<HomeScreen> {
         _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
   }
 
-  Future<void> fetchData() async {
+  Future<List<Results>> fetchData() async {
+    const String endpoint =
+        'https://takatari-cleanup.bubbleapps.io/version-test/api/1.1/obj/Test';
+
+    try {
+      final response = await http.get(Uri.parse(endpoint));
+
+      if (response.statusCode == 200) {
+        objectBox.deleteData(); //
+        waitListEmails.clear();
+        final data = json.decode(response.body);
+        var allRecord = data["response"]["results"];
+        for (Map<String, dynamic> index in allRecord) {
+          waitListEmails.add(Results.fromJson(index));
+        }
+        // dataCount = dataCount + waitListEmails.length;
+        for (int i = 0; i < waitListEmails.length; i++) {
+          objectBox.addUser(
+            UserModelObject(
+              internalId: 0,
+              userId: 1,
+              email: waitListEmails[i].email,
+              mobile: '',
+              name: 'T',
+              roll: '',
+            ),
+          );
+        }
+      }
+      return waitListEmails;
+    } catch (e) {
+      print('Exception: $e');
+      return waitListEmails;
+    }
+  }
+
+  Future<void> pushData(int i) async {
     final response = await http.post(
       Uri.parse(apiUrl),
       headers: {
         'Content-Type': 'application/json',
-        // Add any additional headers if required
       },
       body: jsonEncode({
-        'email': objectBox.getData()![i].name.toString(),
+        'email': objectBox.getData()![i].email.toString(),
       }),
     );
 
     if (response.statusCode == 200) {
-      await prefs.setString('addData', 'False');
-      // controller.clear();
-      // objectBox.deleteData();
-      // If server returns an OK response, parse the JSON
-      setState(() {
-        data = json.decode(response.body);
-      });
+      if (prefs.getString('saveData') == 'True') {
+        await prefs.setString('addData', 'False');
+        objectBox.deleteData();
+        waitListEmails.clear();
+        await fetchData();
+        setState(() {});
+      }
     } else {
-      // If the server did not return a 200 OK response,
-      // throw an exception.
       throw Exception('Failed to load data');
     }
   }
@@ -70,7 +104,6 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> initConnectivity() async {
-    prefs = await SharedPreferences.getInstance();
     late ConnectivityResult result;
     try {
       result = await _connectivity.checkConnectivity();
@@ -82,36 +115,37 @@ class _HomeScreenState extends State<HomeScreen> {
       return Future.value(null);
     }
 
+    if ((result == ConnectivityResult.wifi ||
+            result == ConnectivityResult.mobile) &&
+        prefs.getString('firstTime') == 'True') {
+      await fetchData();
+      await prefs.setString('firstTime', 'True');
+    }
+
     return _updateConnectionStatus(result);
   }
 
   Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    prefs.setString('saveData', 'False');
     final String? action = prefs.getString('addData');
     if ((result == ConnectivityResult.wifi ||
             result == ConnectivityResult.mobile) &&
         action == 'True') {
-      for (i = 0; i < objectBox.getData()!.length; i++) {
-        await fetchData();
-      }objectBox.deleteData();
-      i = 0;
+      for (int i = 0; i < objectBox.getData()!.length; i++) {
+        if (i == objectBox.getData()!.length - 1) {
+          prefs.setString('saveData', 'True');
+        }
+        if (objectBox.getData()![i].name == 'F') {
+          await pushData(i);
+        }
+      }
+      objectBox.deleteData();
+      waitListEmails.clear();
+      await fetchData();
     }
     setState(() {
       _connectionStatus = result;
     });
-  }
-
-  Future<void> functionality() async {
-    objectBox.addUser(
-      UserModelObject(
-        internalId: 0,
-        userId: 1,
-        email: 'zayaanwaqar@gmail.com',
-        mobile: '03314841937',
-        name: 'Connection Restored',
-        roll: '123',
-      ),
-    );
-    await prefs.setString('addData', 'False');
   }
 
   @override
@@ -131,7 +165,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          objectBox.getData()![index].name.toString(),
+                          objectBox.getData()![index].email.toString(),
                           style: const TextStyle(fontSize: 18),
                         ),
                       ],
@@ -142,7 +176,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       height: 10,
                     );
                   },
-                  itemCount: objectBox.getData()?.length ?? 0,
+                  itemCount: objectBox.getData()!.length,
                 ),
               ),
             ),
@@ -162,21 +196,23 @@ class _HomeScreenState extends State<HomeScreen> {
                     UserModelObject(
                       internalId: 0,
                       userId: 1,
-                      email: 'zayaanwaqar@gmail.com',
+                      email: controller.text,
                       mobile: '03314841937',
-                      name: controller.text,
+                      name: 'F',
                       roll: '123',
                     ),
                   );
+
                   await prefs.setString('addData', 'True');
                   final String? action = prefs.getString('addData');
                   if ((_connectionStatus == ConnectivityResult.wifi ||
                           _connectionStatus == ConnectivityResult.mobile) &&
                       action == 'True') {
-                    for (i = 0; i < objectBox.getData()!.length; i++) {
-                      await fetchData();
-                    }objectBox.deleteData();
-                    i = 0;
+                    for (int i = 0; i < objectBox.getData()!.length; i++) {
+                      if (objectBox.getData()![i].name == 'F') {
+                        await pushData(i);
+                      }
+                    }
                   }
                   controller.clear();
                   setState(() {});
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:object_box_practice/home_screen.dart';
 import 'package:object_box_practice/object_box/object_box.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 late ObjectBox objectBox;
+late SharedPreferences prefs;
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   objectBox = await ObjectBox.create();
+  prefs = await SharedPreferences.getInstance();
+  prefs.setString('firstTime', 'True');
   runApp(const MyApp());
 }
 
